ad057ca4dc4a6884e3069b1673148ab91bb63e0b
fox <vadim.pribylov@gmail.com>
2018-10-25 17:59:41 +0300

    added route search function

--- tpnet.py
+++ tpnet.py
@@ -17,7 +17,7 @@ JIHT RAS / PFUR
 
 import random
 import numpy as np
-from graph_tool.all import *
+import graph_tool as gt
 
 
 class Net:
@@ -31,6 +31,8 @@ class Net:
     vname: graph_tool.PropertyMap (type: string)
         vertex names. Might be absent if no names were
         provided to constructor
+    namelup: dict
+        dictionary that provides index lookup by name
     vload: graph_tool.PropertyMap (type: object)
         current load of each vertex.
     vloadargs: graph_tool.PropertyMap (type: vector<float>)
@@ -61,20 +63,23 @@ class Net:
             iterator of (frequency, offset) of periodic load function, must be
             size of `size`. This influences on how many Passenger-class objects
             to spawn on next iteration. Default: None
+        weight: iter(float)
+            weight property of Net edges. Must be size of `edges` or `size*2`
+            if `edges` is None. Default: 1
         """
 
         # TODO: random edges in graph
 
-        self.g = Graph(directed=False)
+        self.g = gt.Graph(directed=False)
         self.g.add_vertex(size)
 
         if names:
             self.vname = self.g.new_vertex_property('string')
             for item, name in zip(self.g.vertices(), names):
                 self.vname[item] = name
-            self.vnamelup = {
+            self.namelup = {
                 key: value
-                for (key, value) in zip(self.g.get_vertices(), names)
+                for (key, value) in zip(names, self.g.get_vertices())
             }
         if edges:
             edges_indexed = [
@@ -82,8 +87,12 @@ class Net:
                 for name1, name2 in edges
             ]
         else:
+            if 'max_random_edges' in kwargs:
+                max_edges = range(kwargs['max_random_edges'])
+            else:
+                max_edges = range(size*2)
             random_edges = [
-                random.sample(range(size), 2) for _ in range(size*2)
+                random.sample(range(size), 2) for _ in max_edges
             ]
             edges_indexed = [
                 (first, second)
@@ -92,17 +101,78 @@ class Net:
         self.g.add_edge_list(edges_indexed)
 
         self.vload = self.g.new_vertex_property('object')
-        if load in kwargs:
+        if 'load' in kwargs:
             for v, l in zip(self.g.vertices(), kwargs['load']):
                 self.vload[v] = l
         else:
             for v in self.g.vertices():
                 self.vload[v] = []
 
-        if loadargs in kwargs:
+        if 'loadargs' in kwargs:
             self.vloadargs = self.g.new_vertex_property('vector<float>')
             for v, la in zip(self.g.vertices(), kwargs['loadargs']):
                 self.vloadargs[v] = la
+
+        self.vweight = self.g.new_edge_property('float')
+        if 'weight' in kwargs:
+            for e, w in zip(self.g.edges(), kwargs['weight']):
+                self.vweight[e] = w
+        else:
+            for e in self.g.edges():
+                self.vweight[e] = 1
+
+    def get_route(self, src, dst, **kwargs):
+        """
+        Returns list with route from `source` to `destination`
+
+        Arguments
+        ------
+        src: int or str
+            starting point of route, can be vertex name or vertex index.
+
+        dst: int or str
+            final point of route, can be vertex name or vertex index
+
+        Returns
+        ------
+        route: numpy.array
+            numpy array of vertex indices for the route
+        """
+
+        # separate checks to allow source and destination to be
+        # type-independant
+        try:
+            source = int(src)
+        except ValueError:
+            source = self.namelup[src]
+        except TypeError:
+            raise TypeError(
+                'expected source to be vertex name or vertex index. '
+                'got {}'.format(type(src))
+            )
+        try:
+            target = int(dst)
+        except ValueError:
+            target = self.namelup[dst]
+        except TypeError:
+            raise TypeError(
+                'expected destination to be vertex name or vertex index. '
+                'got {}'.format(type(src))
+            )
+
+        route = np.array([target])
+        sg = gt.search.dijkstra_iterator(
+                self.g, self.vweight, source=source, array=True)
+        while route[0] != source:
+            found = False
+            for check_src, check_dest in sg:
+                if check_dest == target:
+                    route = np.append(check_src, route)
+                    target = check_src
+                    found = True
+            if not found:
+                raise RuntimeError('cannot find route')
+        return route
 
 
 class Car:
@@ -152,6 +222,7 @@ class Car:
             self.inside = 0
 
         # check if route contains names
+        # TODO: convert into forgivable check
         if isinstance(route[0], str):
             # now check if it is actually integers in string form
             try:
