26754b60f84ce37313d32ce8b5af0140e1a3e913
fox <vadim.pribylov@gmail.com>
2018-10-25 14:18:36 +0300

    added `Net` class

--- tpnet.py
+++ tpnet.py
@@ -7,8 +7,8 @@ Requires:
     numpy
     graph_tool.all
 
-Functions:
-    init - initialize transport net
+Classes:
+    Net - transport net class
 
 ------
 a: Vadim Pribylov, 2018
@@ -20,78 +20,79 @@ import numpy as np
 from graph_tool.all import *
 
 
-def init(size, names=None, edges=None, **kwargs):
+class Net(self):
     """
-    Initialize graph
+    Transport network class
 
-    Args
-    ------
-    size: int
-        number of vertices in graph
-    names: iter(str)
-        list of vertex names, optional
-    edges: iter(tuple(str, str))
-        list of edges between vertices
-
-    KWArgs
-    ------
-    max_random_edges: int
-        amount of random edges to generate. Default: `size`*2
-    load: iter(float)
-        iterator of starting load for vertices. Default: 0
-    loadargs: iter(tuple(float, float))
-        iterator of (frequency, offset) of periodic load function, must be size
-        of `size`. Default: None
-
-    Returns: tuple(graph, net)
-    ------
-    graph: graph_tool.Graph
-        resulting graph
+    TODO: add more text
     """
 
-    # TODO: random edges in graph
+    def __init__(self, size, names=None, edges=None, **kwargs):
+        """
+        Initialize Net
 
-    g = Graph(directed=False)
-    g.add_vertex(size)
+        Args
+        ------
+        size: int
+            number of vertices in graph
+        names: iter(str)
+            list of vertex names, optional
+        edges: iter(tuple(str, str))
+            list of edges between vertices
 
-    if names:
-        vname = g.new_vertex_property('string')
-        for item, name in zip(g.vertices(), names):
-            vname[item] = name
-        vnamelup = {
-            key: value for (key, value) in zip(g.get_vertices(), names)
-        }
-    if edges:
-        edges_indexed = [
-            (vnamelup[name1], vnamelup[name2]) for name1, name2 in edges
-        ]
-    else:
-        random_edges = [random.sample(range(size), 2) for _ in range(size*2)]
-        edges_indexed = [
-            (first, second)
-            for first, second in random_edges
-        ]
-    g.add_edge_list(edges_indexed)
+        KWArgs
+        ------
+        max_random_edges: int
+            amount of random edges to generate. Default: `size`*2
+        load: iter(float)
+            iterator of starting load for vertices. Default: 0
+        loadargs: iter(tuple(float, float))
+            iterator of (frequency, offset) of periodic load function, must be
+            size of `size`. Default: None
 
-    vload = g.new_vertex_property('short')
-    if load in kwargs:
-        for v, l in zip(g.vertices(), kwargs['load']):
-            vload[v] = l
-    else:
-        for v in g.vertices():
-            vload[v] = 0
+        Returns: tuple(graph, net)
+        ------
+        graph: graph_tool.Graph
+            resulting graph
+        """
 
-    if loadargs in kwargs:
-        vloadargs = g.new_vertex_property('vector<float>')
-        for v, la in zip(g.vertices(), kwargs['loadargs']):
-            vloadargs[v] = la
+        # TODO: random edges in graph
 
-    net = {'g': g}
-    if vname:
-        net['vname'] = vname
-        net['vnamelup'] = vnamelup
-    net['vload'] = vload
-    if vloadargs:
-        net['vloadargs'] = vloadargs
+        self.g = Graph(directed=False)
+        self.g.add_vertex(size)
 
-    return net
+        if names:
+            self.vname = self.g.new_vertex_property('string')
+            for item, name in zip(self.g.vertices(), names):
+                self.vname[item] = name
+            self.vnamelup = {
+                key: value
+                for (key, value) in zip(self.g.get_vertices(), names)
+            }
+        if edges:
+            edges_indexed = [
+                (self.vnamelup[name1], self.vnamelup[name2])
+                for name1, name2 in edges
+            ]
+        else:
+            random_edges = [
+                random.sample(range(size), 2) for _ in range(size*2)
+            ]
+            edges_indexed = [
+                (first, second)
+                for first, second in random_edges
+            ]
+        self.g.add_edge_list(edges_indexed)
+
+        self.vload = self.g.new_vertex_property('short')
+        if load in kwargs:
+            for v, l in zip(self.g.vertices(), kwargs['load']):
+                self.vload[v] = l
+        else:
+            for v in self.g.vertices():
+                self.vload[v] = 0
+
+        if loadargs in kwargs:
+            self.vloadargs = self.g.new_vertex_property('vector<float>')
+            for v, la in zip(self.g.vertices(), kwargs['loadargs']):
+                self.vloadargs[v] = la
